76a360c1e3f51fe99d0a8a251df26dea6282ee00
Akandwanaho Nathan <nathankandwanaho3@gmail.com>
2026-05-14 06:16:50 -0700

    updates

--- frontend-project/src/pages/insertTrainnee.jsx
+++ frontend-project/src/pages/insertTrainnee.jsx
@@ -12,45 +12,60 @@ export default function TraineInsert() {
     try {
         const{Trainee_Id,FirstNames,LastName,Gender,Trade_Id}=req.body
      if (!Trainee_Id || !FirstNames || !LastName || !Gender || !Trade_Id) {
-         return res.status(403).json({message:"fill out missing"})
+       return alert("Fill out all fields")
      }
      const res=await axios.post('http://localhost:5000/Api/InsertTrainees',{Trainee_Id,FirstNames,LastName,Gender,Trade_Id});
      res.data.message
+      setTrainee_Id("")
+        setFirstNames("")
+        setLastName("")
+        setGender("")
+        setTrade_Id("")
 
     } catch (err) {
         console.log(err);
-        alert(res.data.Error)
+        alert(err)
         
     }
 }
 
 return(
-    <div className='bg-green-400 justify-center items-center min-h-screen'>
-        <div className='bg-gray-500 rounded-lg shadow-lg'>
-            <h1>REGISTER TRAINEE</h1>
+    <div className='bg-gray-40  flex justify-center items-center min-h-screen'>
+        <div className='bg-gray-100 p-4 w-[600px] rounded-lg shadow-2xl'>
+            <h1 className='text-center text-green-500 mt-3 mb-3 font-bold '>REGISTER TRAINEE</h1>
 
             
                 <input type="text" value={Trainee_Id} placeholder='Enter the trainee Id'
-                  onChange={(e)=>{setFirstNames(e.target.value)}}
-                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold'/>
+                  onChange={(e)=>{setTrainee_Id(e.target.value)}}
+                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold 
+                p-3 mb-3 rounded-lg mt-3
+                 focus:text-green-500'/>
             
                 <input type="text" value={FirstNames} placeholder='Enter the FirstNames '
-                  onChange={(e)=>{setTrainee_Id(e.target.value)}}
-                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold'/>
+                  onChange={(e)=>{setFirstNames(e.target.value)}}
+                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold 
+                p-3 mb-3 rounded-lg mt-3
+                 focus:text-green-500'/>
             
                 <input type="text" value={LastName} placeholder='Enter the LastName '
                   onChange={(e)=>{setLastName(e.target.value)}}
-                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold'/>
+                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold 
+                p-3 mb-3 rounded-lg mt-3
+                 focus:text-green-500'/>
             
                 <input type="text" value={Gender} placeholder='Enter the Gender '
                   onChange={(e)=>{setGender(e.target.value)}}
-                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold'/>
+                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold 
+                p-3 mb-3 rounded-lg mt-3
+                 focus:text-green-500'/>
             
                 <input type="text" value={Trade_Id} placeholder='Enter the Trade_Id '
                   onChange={(e)=>{setTrade_Id(e.target.value)}}
-                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold'/>
+                className='w-full border focus:outline-none focus:ring-2 focus:ring-green-500 tex-green-500 font-bold 
+                p-3 mb-3 rounded-lg mt-3
+                 focus:text-green-500'/>
 
-                <button className='bg-green-500 hover:bg-green-700 transition duration-600 text-white font-bold'>ADD TRAINEE</button>
+                <button onClick={trainee} className='bg-green-500 hover:bg-green-700 transition duration-600 mb-3 text-white font-bold p-3 mt-3 rounded-lg mt-3 w-full'>ADD TRAINEE</button>
             
         </div>
     </div>
